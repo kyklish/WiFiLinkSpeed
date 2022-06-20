004a817dcb98ea609d8a28ea508380b75502e85a
kyklish <kyklish@gmail.com>
2022-06-20 21:50:42 +0300

    move all notification code to 'NotificationHolder' class

--- app/src/main/java/com/example/wifilinkspeed/ForegroundService.kt
+++ app/src/main/java/com/example/wifilinkspeed/ForegroundService.kt
@@ -1,10 +1,5 @@
 package com.example.wifilinkspeed
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
-import android.app.PendingIntent.getActivity
 import android.app.Service
 import android.content.Intent
 import android.graphics.Color
@@ -21,7 +16,6 @@ import java.util.*
 
 class ForegroundService : Service() {
 	companion object {
-		private const val REQUEST_CODE = 0
 		private const val NOTIFICATION_ID = 1001 // must not be 0
 
 		// Android updates wifi info approximately every second
@@ -29,8 +23,7 @@ class ForegroundService : Service() {
 	}
 
 	private lateinit var wifiInfo: WiFiInfo
-	private lateinit var notificationManager: NotificationManager
-	private lateinit var notificationBuilder: Notification.Builder
+	private lateinit var notificationHolder: NotificationHolder
 	private var timer: Timer? = null
 	private var handler: Handler? = null
 	private var handlerTask: Runnable? = null
@@ -52,13 +45,10 @@ class ForegroundService : Service() {
 
 	private fun initializeService() {
 		wifiInfo = WiFiInfo(this)
-		notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-		createNotificationChannel()
-		notificationBuilder =
-			Notification.Builder(this, getString(R.string.notification_channel_id))
-		val notification = createNotification(getString(R.string.notification_message))
+		// if you pass 'applicationContext', notification will live longer, then we need it
+		notificationHolder = NotificationHolder(this, NOTIFICATION_ID)
 		// make service alive forever
-		startForeground(NOTIFICATION_ID, notification)
+		startForeground(NOTIFICATION_ID, notificationHolder.notification)
 		// start main work
 //		updateUIbyTimer() // works fine, no problems
 		updateUIbyHandler() // more reliable by StackOverflow
@@ -107,42 +97,6 @@ class ForegroundService : Service() {
 		return super.onStartCommand(intent, flags, startId)
 	}
 
-	private fun createNotificationChannel() {
-		val channel = NotificationChannel(
-			getString(R.string.notification_channel_id),
-			getString(R.string.notification_channel_name),
-			NotificationManager.IMPORTANCE_LOW // No sound
-//			NotificationManager.IMPORTANCE_MIN // No sound and does not appear in the status bar
-		).apply {
-			description = getString(R.string.notification_description)
-		}
-
-		notificationManager.createNotificationChannel(channel)
-	}
-
-	private fun createNotification(message: String): Notification {
-		val contentIntent = Intent(applicationContext, MainActivity::class.java)
-		val contentPendingIntent = getActivity(
-			applicationContext,
-			REQUEST_CODE,
-			contentIntent,
-			FLAG_UPDATE_CURRENT
-		)
-
-		return notificationBuilder
-			.setSmallIcon(R.drawable.ic_wifi_small_icon)
-//			.setContentTitle(getString(R.string.notification_title))
-			.setContentText(message)
-//			.setSubText(getString(R.string.notification_sub_text))
-//			.setTicker(getString(R.string.notification_ticker_text))
-			// On lock screen shows the notification's full content
-			.setVisibility(Notification.VISIBILITY_PUBLIC)
-			// if launch activity from notification not work read this:
-			// "android - How To Start An Activity From Background in Android 10 (READ LAST COMMENT deep linking) - Stack Overflow.rar"
-			.setContentIntent(contentPendingIntent)
-			.build()
-	}
-
 	@Suppress("unused")
 	private fun updateUIbyTimer() {
 /*		if (BuildConfig.DEBUG) {
@@ -188,15 +142,10 @@ class ForegroundService : Service() {
 	private fun updateUI() {
 		val linkSpeedStr = wifiInfo.linkSpeedStr(this@ForegroundService)
 		callbackUpdateActivityText?.invoke(linkSpeedStr) // MainActivity
-		notify(linkSpeedStr) // Notification
+		notificationHolder.notify(linkSpeedStr) // Notification
 		overlayWindowD?.setText(linkSpeedStr) // OverlayWindow Debug
 		overlayWindowL?.setText(linkSpeedStr) // OverlayWindow Debug
 		overlayWindow?.setText(linkSpeedStr) // OverlayWindow Release
-	}
-
-	private fun notify(message: String) {
-		notificationBuilder.setContentText(message)
-		notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
 	}
 
 	fun registerCallbackForResults(callback: (String) -> Unit) {
---
+++ app/src/main/java/com/example/wifilinkspeed/NotificationHolder.kt
@@ -0,0 +1,79 @@
+package com.example.wifilinkspeed
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+
+class NotificationHolder(
+	private val context: Context,
+	private val NOTIFICATION_ID: Int
+) {
+	companion object {
+		private const val REQUEST_CODE = 0
+	}
+
+	val notification: Notification
+	private var notificationManager =
+		context.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+	private var notificationBuilder: Notification.Builder
+
+	init {
+		createNotificationChannel()
+		notificationBuilder =
+			Notification.Builder(context, getString(R.string.notification_channel_id))
+		notification = createNotification(
+			context,
+			notificationBuilder,
+			getString(R.string.notification_message)
+		)
+	}
+
+	private fun createNotificationChannel() {
+		val channel = NotificationChannel(
+			getString(R.string.notification_channel_id),
+			getString(R.string.notification_channel_name),
+			NotificationManager.IMPORTANCE_LOW // No sound
+//			NotificationManager.IMPORTANCE_MIN // No sound and does not appear in the status bar
+		).apply {
+			description = getString(R.string.notification_description)
+		}
+
+		notificationManager.createNotificationChannel(channel)
+	}
+
+	private fun createNotification(
+		context: Context,
+		notificationBuilder: Notification.Builder,
+		message: String
+	): Notification {
+		val contentIntent = Intent(context, MainActivity::class.java)
+		val contentPendingIntent = PendingIntent.getActivity(
+			context,
+			REQUEST_CODE,
+			contentIntent,
+			PendingIntent.FLAG_UPDATE_CURRENT
+		)
+
+		return notificationBuilder
+			.setSmallIcon(R.drawable.ic_wifi_small_icon)
+//			.setContentTitle(getString(R.string.notification_title))
+			.setContentText(message)
+//			.setSubText(getString(R.string.notification_sub_text))
+//			.setTicker(getString(R.string.notification_ticker_text))
+			// On lock screen shows the notification's full content
+			.setVisibility(Notification.VISIBILITY_PUBLIC)
+			// if launch activity from notification not work read this:
+			// "android - How To Start An Activity From Background in Android 10 (READ LAST COMMENT deep linking) - Stack Overflow.rar"
+			.setContentIntent(contentPendingIntent)
+			.build()
+	}
+
+	fun notify(message: String) {
+		notificationBuilder.setContentText(message)
+		notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+	}
+
+	private fun getString(id: Int): String {
+		return context.resources.getString(id)
+	}
+}
